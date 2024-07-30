4f570db7116b1584ef9dfaa5be9c54c1c65d1a73
Mathieu Lu <mathieu@symbiotic.coop>
2024-07-30 17:13:15 -0400

    upgrade civix

--- cardvault.php
+++ cardvault.php
@@ -44,7 +44,7 @@ function cardvault_civicrm_managed(&$entities) {
       'payment_type' => 1,
     ),
   );
-  return _cardvault_civix_civicrm_managed($entities);
+  return;
 }
 
 /**
@@ -120,18 +120,6 @@ function cardvault_civicrm_buildForm($formName, &$form) {
   }
 }
 
-
-/**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function cardvault_civicrm_xmlMenu(&$files) {
-  _cardvault_civix_civicrm_xmlMenu($files);
-}
-
 /**
  * Implements hook_civicrm_install().
  *
@@ -146,18 +134,8 @@ function cardvault_civicrm_install() {
 *
 * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
 */
-function cardvault_civicrm_postInstall() {
-  _cardvault_civix_civicrm_postInstall();
-}
 
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function cardvault_civicrm_uninstall() {
-  _cardvault_civix_civicrm_uninstall();
-}
+
 
 /**
  * Implements hook_civicrm_enable().
@@ -166,31 +144,6 @@ function cardvault_civicrm_uninstall() {
  */
 function cardvault_civicrm_enable() {
   _cardvault_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function cardvault_civicrm_disable() {
-  _cardvault_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function cardvault_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _cardvault_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
@@ -209,44 +162,6 @@ function cardvault_civicrm_alterMailParams(&$params, $context) {
       $params['tplParams']['cc_number'] = $ccInfo['cc_number'];
     }
   }
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * @param array $caseTypes
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function cardvault_civicrm_caseTypes(&$caseTypes) {
-  _cardvault_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function cardvault_civicrm_angularModules(&$angularModules) {
-_cardvault_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function cardvault_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _cardvault_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
